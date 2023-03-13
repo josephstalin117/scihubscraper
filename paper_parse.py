2c05ec8623ae676eb5d56f6a7176bf0bd09ddad9
josephlin <josephlin117@hotmail.com>
2023-03-13 11:59:44 +0800

    update paper_parse

---
+++ paper_parse.py
@@ -0,0 +1,11 @@
+import sys, fitz
+
+print("test")
+fname = "test.pdf"
+doc = fitz.open(fname)  # open document
+out = open(fname + ".txt", "wb")  # open text output
+for page in doc:  # iterate the document pages
+    text = page.get_text().encode("utf8")  # get plain text (is in UTF-8)
+    out.write(text)  # write text of page
+    out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
+out.close()
